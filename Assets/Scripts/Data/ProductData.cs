7191b8ee2801a54a93e92896dfbc1afc0858cd3e
Terentev Kirill <mowder96@gmail.com>
2020-09-13 17:55:15 +0500

    Test data

--- Assets/Scripts/Data/ProductData.cs
+++ Assets/Scripts/Data/ProductData.cs
@@ -8,11 +8,14 @@ public class ProductData : ScriptableObject
 	[SerializeField]
 	private int value;
 	[SerializeField]
+	private ProductType type;
+	[SerializeField]
 	private string description;
 	private bool canBuy = true;
 
 	public string Id => id;
 	public int Value => value;
+	public ProductType Type => type;
 	public string Description => description;
 	public bool CanBuy => canBuy;
 }
